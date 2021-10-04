0d0d67cf36bcc603d04cf5a3e8841c2733eed7a0
Justin Jaffray <justin.jaffray@gmail.com>
2021-10-04 13:50:38 -0700

    add benchmarks

--- benches/identity.rs
+++ benches/identity.rs
@@ -6,16 +6,21 @@ use std::sync::mpsc::channel;
 use std::thread::{self, sleep};
 use std::time::Duration;
 
-fn benchmark_identity() {
+const NUM_OPS: usize = 20;
+const NUM_INTS: usize = 1000000;
+
+// This benchmark runs babyflow which more-or-less just copies the data directly
+// between the operators, but with some extra overhead.
+fn benchmark_babyflow(num_ops: usize, num_ints: usize) {
     let mut q = Query::new();
 
-    let mut op = q.source(|send| {
-        for i in 0..100000 {
+    let mut op = q.source(move |send| {
+        for i in 0..num_ints {
             send.push(i);
         }
     });
 
-    for _ in 0..20 {
+    for _ in 0..num_ops {
         op = op.map(|i| i);
     }
 
@@ -26,9 +31,70 @@ fn benchmark_identity() {
     (*q.df).borrow_mut().run();
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("identity", |b| b.iter(|| benchmark_identity()));
+fn criterion_babyflow(c: &mut Criterion) {
+    c.bench_function("babyflow", |b| {
+        b.iter(|| benchmark_babyflow(NUM_OPS, NUM_INTS))
+    });
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+// This benchmark creates a thread for each operator and has them send data between each other via channels.
+fn benchmark_pipeline(num_ops: usize, num_ints: usize) {
+    let (input, mut output) = channel();
+
+    for _ in 0..num_ops {
+        let (tx, mut rx) = channel();
+        std::mem::swap(&mut output, &mut rx);
+        thread::spawn(move || {
+            for elt in rx {
+                tx.send(elt).unwrap();
+            }
+        });
+    }
+
+    for i in 0..num_ints {
+        input.send(i).unwrap();
+    }
+    drop(input);
+    for elt in output {
+        black_box(elt);
+    }
+}
+
+fn criterion_pipeline(c: &mut Criterion) {
+    c.bench_function("pipeline", |b| {
+        b.iter(|| benchmark_pipeline(NUM_OPS, NUM_INTS))
+    });
+}
+
+// This benchmark just copies around a bunch of data with basically zero
+// overhead, so this should theoretically be the fastest achievable (with a
+// single thread).
+fn benchmark_speed_of_light(num_ops: usize, num_ints: usize) {
+    let mut data: Vec<_> = (0..num_ints).collect();
+
+    for _ in 0..num_ops {
+        let mut next = Vec::new();
+        for v in data.drain(..) {
+            next.push(v);
+        }
+        data = next;
+    }
+
+    for elt in data {
+        black_box(elt);
+    }
+}
+
+fn criterion_speed_of_light(c: &mut Criterion) {
+    c.bench_function("speed of light", |b| {
+        b.iter(|| benchmark_speed_of_light(NUM_OPS, NUM_INTS))
+    });
+}
+
+criterion_group!(
+    identity_dataflows,
+    criterion_babyflow,
+    criterion_pipeline,
+    criterion_speed_of_light,
+);
+criterion_main!(identity_dataflow);
